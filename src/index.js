ba1a9be6cfee02d8351e216917845c0907954b3b
Gullit Moran <gullitmoran@hotmail.com>
2023-08-24 11:02:47 -0500

    Cambio de URL deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store/store';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://back-countries-qid0-dev.fl0.io/';
+axios.defaults.baseURL = 'https://back-countries-0k7m-dev.fl0.io/';
 
 ReactDOM.render(
   <React.StrictMode>
